7f03533089d8fe0c83e436081e76ce59eb93ab61
pankajk1995 <5006pankajkmaurya@gmail.com>
2024-06-04 21:14:55 +0530

    update

---
+++ 01hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Hello, World!\n");
+    printf("pankaj maurya");
+
+    // Return 0 to indicate successful completion of the program
+    return 0;
+}
